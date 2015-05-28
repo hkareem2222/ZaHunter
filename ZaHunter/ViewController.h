551e5e242893ab2c0e6f7be0cd96174afbb6c3e5
Husein Kareem <huseinkareem@gmail.com>
2015-05-28 13:24:31 -0500

    Initial Commit

---
+++ ZaHunter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZaHunter
+//
+//  Created by Husein Kareem on 5/28/15.
+//  Copyright (c) 2015 Husein Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZaHunter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZaHunter
+//
+//  Created by Husein Kareem on 5/28/15.
+//  Copyright (c) 2015 Husein Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
